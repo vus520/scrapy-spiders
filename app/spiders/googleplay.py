3f25d7ba00e805fd82427e5360869fee6ccbf514
尘缘 <admin@4wei.cn>
2016-07-18 20:50:40 +0800

    优化url限制

--- app/spiders/googleplay.py
+++ app/spiders/googleplay.py
@@ -15,9 +15,8 @@ class GoogleplaySpider(CrawlSpider):
         'https://play.google.com/store/apps/details?id=com.viber.voip'
     ]
     rules = [
-        Rule(LinkExtractor(allow=("https://play\.google\.com/store/apps/details", )), callback='parse_app', follow=True),
+        Rule(LinkExtractor(allow=("https://play\.google\.com/store/apps/details\?id=[\w\.]+", )), callback='parse_app', follow=True),
     ] # CrawlSpider 会根据 rules 规则爬取页面并调用函数进行处理
-
 
     def parse_app(self, response):
         item = GoogleItem()
@@ -25,9 +24,11 @@ class GoogleplaySpider(CrawlSpider):
         item['num']    = response.xpath("//div[@itemprop='numDownloads']").xpath("text()").extract()
         item['cate']   = response.xpath("//span[@itemprop='genre']").xpath("text()").extract()
         item['rate']   = response.xpath("//div[@itemprop='contentRating']").xpath("text()").extract()
-        item['desc']   = response.xpath("//div[@class='description']").xpath("text()").extract()
+        item['desc']   = response.xpath("//div[@class='description']").extract()
+        item['desc']   = "\n".join(item['desc'])
         item['score']  = response.xpath("//div[@class='score']").xpath("text()").extract()
         item['meta']   = response.xpath("//meta[@name='description']").xpath("@content").extract()
+        item['id']     = response.xpath("/html").re(u'data-docid="(.*?)"')[0]
         item['info']   = response.xpath("//div").re(u'<div class="title">([^<]+)</div>\s*?<div class="content">([^<]+)</div>')
         yield item
 
